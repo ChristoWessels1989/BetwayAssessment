136579b603d42aec7f4904418ee3d97f10d6666a
christowessels <christowessels@gmail.com>
2025-02-17 17:11:18 +0200

    add result class for wager request amd playerservice methods

---
+++ src/OT.Assessment.App/Models/DTOs/WagerResultDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OT.Assessment.App.Models.DTOs
+{
+  public class WagerResultDTO
+  {
+    public Guid WagerId { get; set; }
+    public string Game { get; set; }
+    public string Provider { get; set; }
+    public double Amount { get; set; }
+    public DateTime CreatedDateTime { get; set; }
+  }
+}
--- src/OT.Assessment.App/Services/PlayerService.cs
+++ src/OT.Assessment.App/Services/PlayerService.cs
@@ -1,13 +1,42 @@
-﻿using OT.Assessment.App.Models.DTOs;
+﻿using AutoMapper;
+using OT.Assessment.App.AsyncDataServices.Interfaces;
+using OT.Assessment.App.Models;
+using OT.Assessment.App.Models.DTOs;
 using OT.Assessment.App.Services.Interfaces;
 
 namespace OT.Assessment.App.Services
 {
   public class PlayerService : IPlayerService
   {
-    public Task<BaseResponseDTO> CreateWager(WagerPublishDTO wagerPublishDTO)
+    private readonly IMapper _mapper;
+    private readonly IMessageBusClient _messageBusClient;
+    public PlayerService(
+          IMapper mapper,
+          IMessageBusClient messageBusClient)
     {
-      throw new NotImplementedException();
+      _mapper = mapper;
+      _messageBusClient = messageBusClient;
+    }
+    public async Task<BaseResponseDTO> CreateWager(WagerPublishDTO wagerPublishDTO)
+    {
+      var response = new BaseResponseDTO();
+      var casinoWager = _mapper.Map<CasinoWager>(wagerPublishDTO);
+
+      //Send Async Message
+      try
+      {
+        _messageBusClient.PublishNewWager(casinoWager);
+        response.IsSuccess = true;
+        response.Message = $"Wager with ID :{casinoWager.WagerId} published";
+
+      }
+      catch (Exception ex)
+      {
+        response.IsSuccess = false;
+        response.Message = $"Wager with ID :{casinoWager.WagerId} failed to publish";
+      }
+
+      return response;
     }
   }
 }
